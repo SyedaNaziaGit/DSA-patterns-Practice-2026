119bba3115074fa016e93523a69f2876b2fcd329
Syeda Nazia <142724168+SyedaNaziaGit@users.noreply.github.com>
2026-02-10 17:06:14 +0530

    Add files via upload

---
+++ two_pointers/4_leetcode_344-Reverse String.py
@@ -0,0 +1,21 @@
+'''
+Docstring for two_pointers.4_leetcode_344-Reverse String
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+'''
+from typing import List
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        l,r = 0, len(s)-1
+        while l < r:
+            s[l],s[r]= s[r],s[l]
+            l += 1
+            r -= 1
