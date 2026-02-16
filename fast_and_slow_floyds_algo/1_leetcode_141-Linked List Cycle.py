ddb056e3612866052a1a862a476988212c626e4a
Syeda Nazia <142724168+SyedaNaziaGit@users.noreply.github.com>
2026-02-16 13:14:50 +0530

    Add files via upload

---
+++ fast_and_slow_floyds_algo/1_leetcode_141-Linked List Cycle.py
@@ -0,0 +1,40 @@
+'''
+Docstring for 1_leetcode_141-Linked List Cycle
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+'''
+from typing import Optional
+#singly-linked list class
+class ListNode:
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+class Solution:
+    def hasCycle(self,head:Optional[ListNode]) -> bool:
+        if not head:
+            #check if head is none - no linkedlist then return false
+            return False
+        #using two pointers- floyds tortise and hare algorithm
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next# iterating one step
+            fast = fast.next.next #iterating two step
+            #if both meet then cycle is detected
+            if slow == fast:
+                return True
+        return False
+    #runs with O(1) Space Complexity and O(n) Time Complexity
